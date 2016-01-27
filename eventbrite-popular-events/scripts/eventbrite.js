2883ce09bd3389873c96fabf36afcebf34e7d646
saurabhsood91 <saurabhsood91@gmail.com>
2016-01-27 16:54:27 -0700

    auto run oauth script

--- eventbrite-popular-events/scripts/eventbrite.js
+++ eventbrite-popular-events/scripts/eventbrite.js
@@ -1,10 +1,8 @@
 $(function(){
-  $('#btnAuth').click(function(){
-    chrome.identity.launchWebAuthFlow({
-      'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
-      'interactive': true
-    },function(redirect_url) {
-      alert(redirect_url);
-    });
+  chrome.identity.launchWebAuthFlow({
+    'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
+    'interactive': true
+  },function(redirect_url) {
+    alert(redirect_url);
   });
 });
