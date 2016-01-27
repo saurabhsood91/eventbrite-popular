b1d5283f9afef4a65f78fb4f8d82e6ef03e1f7eb
saurabhsood91 <saurabhsood91@gmail.com>
2016-01-27 16:35:24 -0700

    initial oauth workflow

--- eventbrite-popular-events/scripts/eventbrite.js
+++ eventbrite-popular-events/scripts/eventbrite.js
@@ -1,3 +1,10 @@
 $(function(){
-  document.write("Hello, World");
+  $('#btnAuth').click(function(){
+    chrome.identity.launchWebAuthFlow({
+      'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
+      'interactive': true
+    },function(redirect_url) {
+      alert(redirect_url);
+    });
+  });
 });
