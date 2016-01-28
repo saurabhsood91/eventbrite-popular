da6297957ca449a8985c103d8a47848a2682e8d0
saurabhsood91 <saurabhsood91@gmail.com>
2016-01-27 17:39:18 -0700

    add angular and retrieve oauth token

--- eventbrite-popular-events/scripts/eventbrite.js
+++ eventbrite-popular-events/scripts/eventbrite.js
@@ -1,8 +1,24 @@
-$(function(){
-  chrome.identity.launchWebAuthFlow({
-    'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
-    'interactive': true
-  },function(redirect_url) {
-    alert(redirect_url);
-  });
+// $(function(){
+//   chrome.identity.launchWebAuthFlow({
+//     'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
+//     'interactive': true
+//   },function(redirect_url) {
+//     alert(redirect_url);
+//   });
+// });
+angular.module('eventbrite', [])
+.controller('MainController', function(){
+  var self = this;
+  var initialize = function() {
+    // See if you can store token in local storage
+    // Else retrieve through OAuth flow
+    chrome.identity.launchWebAuthFlow({
+      'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
+      'interactive': true
+    },function(redirect_url) {
+      self.token = redirect_url.split("&")[1].split("=")[1];
+    });
+  };
+  initialize();
+
 });
