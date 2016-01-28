73a121b9396d0a5652c4d454a74b92ce42ebae3d
saurabhsood91 <saurabhsood91@gmail.com>
2016-01-27 17:53:44 -0700

    add local storage to store token

--- eventbrite-popular-events/scripts/eventbrite.js
+++ eventbrite-popular-events/scripts/eventbrite.js
@@ -12,13 +12,25 @@ angular.module('eventbrite', [])
   var initialize = function() {
     // See if you can store token in local storage
     // Else retrieve through OAuth flow
-    chrome.identity.launchWebAuthFlow({
-      'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
-      'interactive': true
-    },function(redirect_url) {
-      self.token = redirect_url.split("&")[1].split("=")[1];
+    chrome.storage.local.get('token', function(objects){
+      if(objects.token === undefined) {
+        chrome.identity.launchWebAuthFlow({
+          'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=TTNJP3XC3TXKXN625J',
+          'interactive': true
+        },function(redirect_url) {
+          self.token = redirect_url.split("&")[1].split("=")[1];
+          // Set the token to local storage
+          chrome.storage.local.set({
+            'token': self.token
+          }, function(){
+            console.log('Token Saved');
+          });
+        });
+      } else {
+        // Set token on scope
+        self.token = objects.token
+      }
     });
   };
   initialize();
-
 });
