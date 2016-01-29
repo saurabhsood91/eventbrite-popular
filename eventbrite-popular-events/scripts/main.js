e50198beee25bdec249b1981176cff1f1f7d51f5
saurabhsood91 <saurabhsood91@gmail.com>
2016-01-29 11:27:18 -0700

    add fullscreen support

--- eventbrite-popular-events/scripts/main.js
+++ eventbrite-popular-events/scripts/main.js
@@ -1,6 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('../popup.html',
     {
-      "id": "index"
+      "id": "index",
+      "state": "maximized"
     });
 });
