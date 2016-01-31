569e46b5e55d3e1f1295a488cf39f45b0e5e102e
saurabhsood91 <saurabhsood91@gmail.com>
2016-01-30 19:45:53 -0700

    refactor and add html5 geolocation support

--- eventbrite-popular-events/scripts/eventbrite.js
+++ eventbrite-popular-events/scripts/eventbrite.js
@@ -5,6 +5,7 @@ angular.module('eventbrite', ['angularSpinner'])
   self.locationQuery = '';
   self.hasData = false;
   self.showSpinner = false;
+  // self.isLocationRadio = false;
   var initialize = function() {
     // See if you can store token in local storage
     // Else retrieve through OAuth flow
@@ -29,44 +30,59 @@ angular.module('eventbrite', ['angularSpinner'])
     });
   };
 
+  self.getLocationEvents = function() {
+    if(self.isLocationRadio) {
+      var geolocation = navigator.geolocation;
+      geolocation.getCurrentPosition(function(position){
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        self.showSpinner = true;
+        EventbriteAPIService.getPopularEventsByCoords(lat, long, 0, self.token, self.showData)
+      }, function(err){
+        console.log(err);
+      });
+    }
+  };
+
+  self.showData = function(data) {
+    // TODO validate the data
+    self.eventData = data;
+    // Store the page count
+    self.pages = [];
+    for(var i = 1; i <= data.pagination.page_count; i++) {
+      self.pages.push(i);
+    }
+    self.hasData = true;
+    self.showSpinner = false;
+
+    for(var i = 0; i < self.eventData.events.length; i++) {
+      // get the time attribute
+      var event = self.eventData.events[i];
+      event.isOnComingWeekend = false;
+      var new_date = new Date(event.start.utc);
+      // Add it to the data object
+      var event_time = new_date.getTime();
+      // Get current time
+      var time_now = new Date().getTime();
+      // Get difference of the times
+      var diff = event_time - time_now;
+      // Can do actual weekend calculation here
+      if(diff > 0) {
+        // if within the next 7 days and on a saturday and sunday
+        var next_days = 7 * 24 * 3600 * 1000;
+        var day = new_date.getDay();
+        if(diff < next_days && (day === 0 || day == 6)) {
+          event.isOnComingWeekend = true;
+        }
+      }
+    }
+  };
+
   self.getPopularEvents = function(page) {
     // Show Spinner
     self.showSpinner = true;
     // Get list of Popular events
-    EventbriteAPIService.getPopularEvents(self.locationQuery, self.token, page, function(data) {
-      // TODO validate the data
-      self.eventData = data;
-      // Store the page count
-      self.pages = [];
-      for(var i = 1; i <= data.pagination.page_count; i++) {
-        self.pages.push(i);
-      }
-      self.hasData = true;
-      self.showSpinner = false;
-
-      for(var i = 0; i < self.eventData.events.length; i++) {
-        // get the time attribute
-        var event = self.eventData.events[i];
-        event.isOnComingWeekend = false;
-        var new_date = new Date(event.start.utc);
-        // Add it to the data object
-        var event_time = new_date.getTime();
-        // Get current time
-        var time_now = new Date().getTime();
-        // Get difference of the times
-        var diff = event_time - time_now;
-        // Can do actual weekend calculation here
-        if(diff > 0) {
-          // if within the next 7 days and on a saturday and sunday
-          var next_days = 7 * 24 * 3600 * 1000;
-          var day = new_date.getDay();
-          if(diff < next_days && (day === 0 || day == 6)) {
-            event.isOnComingWeekend = true;
-          }
-        }
-      }
-
-    });
+    EventbriteAPIService.getPopularEvents(self.locationQuery, self.token, page, self.showData);
   };
   initialize();
 }])
@@ -92,7 +108,32 @@ angular.module('eventbrite', ['angularSpinner'])
       console.log(err);
     });
   };
+
+  var getPopularEventsByCoords = function(lat, lng, page, token, callback) {
+    // Logic goes here
+    var url = "https://www.eventbriteapi.com/v3/events/search";
+    $http.get(url, {
+      params: {
+        'token': token,
+        'popular': true,
+        'location.latitude': lat,
+        'location.longitude': lng,
+        'sort_by': 'date',
+        'page': page
+      }
+    })
+    .success(function(data){
+      console.log(data);
+      // Pass data back to the controller
+      callback(data);
+    })
+    .error(function(err) {
+      console.log(err);
+    });
+  };
+
   return {
-    getPopularEvents: getPopularEvents
+    getPopularEvents: getPopularEvents,
+    getPopularEventsByCoords: getPopularEventsByCoords
   }
 }]);
