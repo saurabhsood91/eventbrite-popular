41b7cade5e939ae2e20d8d22982019d7ff5b4473
Saurabh Sood <saurabhsood91@gmail.com>
2016-09-27 18:30:42 -0700

    add launchwebauthflow code plus change token

--- eventbrite-events-react/app/index.js
+++ eventbrite-events-react/app/index.js
@@ -5,6 +5,28 @@ var SearchContainer = require('../containers/SearchContainer');
 var TableContainer = require('../containers/TableContainer');
 
 var MainContainer = React.createClass({
+    componentWillMount: function() {
+        var self = this;
+        chrome.storage.local.get('token', function(objects){
+          if(objects.token === undefined) {
+            chrome.identity.launchWebAuthFlow({
+              'url': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=V52WWQLQ73NY5OUXUP',
+              'interactive': true
+            },function(redirect_url) {
+              self.token = redirect_url.split("&")[1].split("=")[1];
+              // Set the token to local storage
+              chrome.storage.local.set({
+                'token': self.token
+              }, function(){
+                console.log('Token Saved');
+              });
+            });
+          } else {
+            // Set token on scope
+            self.token = objects.token;
+          }
+        });
+    },
     render: function() {
         return (
             <div>
